2833b5adc7624580594bf9f99d035cecbcf1487c
takasora1217 <honey821sky@outlook.jp>
2025-05-13 17:58:04 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <span>moji</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
