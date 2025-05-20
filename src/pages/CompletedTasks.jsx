f501c81f946aa908fc50e81af8cbd12bf1587b7d
Iona324 <yori.oto.sai@gmail.com>
2025-05-20 18:00:28 +0900

    fix:タスク完了完了ページ修正

--- src/pages/CompletedTasks.jsx
+++ src/pages/CompletedTasks.jsx
@@ -3,11 +3,21 @@ import React from "react";
 const CompletedTasks = () => {
   return (
     <div>
-      <div class="flex justify-center text-5xl m-20">完了したタスク</div>;
-      <div class="flex items-start justify-center border-2 border-black mx-auto w-64 ">
-        todo済
+      <div class="flex justify-center text-5xl m-20">完了したタスク</div>
+      <div class="space-y-2">
+        <div class="flex justify-center border-2 border-black mx-auto w-64 ">
+          todo済みのタスクが表示されます
+        </div>
+        <div class="flex justify-center border-2 border-black mx-auto w-64 ">
+          todo済みのタスクが表示されます
+        </div>
+        <div class="flex justify-center border-2 border-black mx-auto w-64 ">
+          todo済みのタスクが表示されます
+        </div>
+        <div class="flex justify-center border-2 border-black mx-auto w-64 ">
+          todo済みのタスクが表示されます
+        </div>
       </div>
-      ;
     </div>
   );
 };
