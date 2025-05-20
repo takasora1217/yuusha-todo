ac87d64e6474597ef5895e134c94c987c1d55baa
Iona324 <yori.oto.sai@gmail.com>
2025-05-20 17:20:32 +0900

    git:完了したタスクスの表示完成

---
+++ src/pages/CompletedTasks.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const CompletedTasks = () => {
+  return (
+    <div>
+      <div class="flex justify-center text-5xl m-20">完了したタスク</div>;
+      <div class="flex items-start justify-center border-2 border-black mx-auto w-64 ">
+        todo済
+      </div>
+      ;
+    </div>
+  );
+};
+
+export default CompletedTasks;
