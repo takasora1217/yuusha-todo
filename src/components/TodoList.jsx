00f1c1dedcbf550582dcbb5373fee65b367231e3
Iona324 <yori.oto.sai@gmail.com>
2025-05-20 19:09:02 +0900

    feat:todoリスト作成

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,17 +2,45 @@ import React from "react";
 import { LuSwords } from "react-icons/lu";
 import { FaTrashAlt } from "react-icons/fa";
 
-export const TodoList = () => {
+export const TodoList = ({ taskList, setTaskList }) => {
+  const handleDone = (id) => {
+    setTaskList(
+      taskList.map((task) => {
+        if (id === task.id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      })
+    );
+  };
+
+  const handleDelete = (id) => {
+    setTaskList(taskList.filter((task) => task.id !== id));
+  };
+
   return (
-    <>
-      <div className="flex space-x-1">
-        <div className="border-solid border-2 border-black w-64 h-6">
-          TodoList
-        </div>
-        <LuSwords className="w-6 h-6" />
-        <FaTrashAlt className="w-5 h-5" />
+    <div className="todoList">
+      <div className="todos">
+        {taskList.map((task, index) => (
+          <div
+            className={`todo ${task.completed ? "completed" : ""}`}
+            key={index}
+          >
+            <div className="todoText">
+              <span>{task.text}</span>
+            </div>
+            <div className="icons">
+              <button onClick={() => handleDone(task.id)}>
+                <uSwords />
+              </button>
+              <button onClick={() => handleDelete(task.id)}>
+                <FaRegTrashAlt />
+              </button>
+            </div>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
