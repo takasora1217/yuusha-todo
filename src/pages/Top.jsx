73a69a9848d4970c94495d908cf6c04666ac3941
misatoku <baskeaoi@icloud.com>
2025-05-22 09:21:38 +0900

    fix:ボスと勇者の位置を調整

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import InputForm from "../components/InputForm";
 import boss from "../images/boss.png";
+import yuusha from "../images/yuusha-1.png";
 
 const Top = ({}) => {
   const [taskList, setTaskList] = useState([]);
@@ -9,7 +10,12 @@ const Top = ({}) => {
       <div className="flex justify-center">
         <img src={boss} alt="boss" className="w-64" />
       </div>
-      <InputForm InputForm taskList={taskList} setTaskList={setTaskList} />
+      <div className="flex justify-center">
+        <InputForm InputForm taskList={taskList} setTaskList={setTaskList} />
+      </div>
+      <div className="flex justify-center h-[280px] items-center pt-1">
+        <img src={yuusha} alt="yuusha" className="w-56" />
+      </div>
     </>
   );
 };
