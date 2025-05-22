08d793734371265b97d6640f9cb5b6d2ad34ced9
Iona324 <yori.oto.sai@gmail.com>
2025-05-22 18:36:46 +0900

    feat:Todoリスト機能作成

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react';
-import InputForm from '../components/InputForm';
+import { useState } from "react";
+import InputForm from "../components/InputForm";
+import TodoList from "../components/TodoList";
 
-const Top = ({}) => {
-const [taskList,setTaskList]=useState([]);
-    return (
-        <InputForm InputForm taskList={taskList} setTaskList={setTaskList} />)
-        
-}
+const Top = () => {
+  const [taskList, setTaskList] = useState([]);
+  const [completedCount, setCompletedCount] = useState(0);
+  return (
+    <>
+      <InputForm InputForm taskList={taskList} setTaskList={setTaskList} />
+      <TodoList
+        taskList={taskList}
+        setTaskList={setTaskList}
+        completedCount={completedCount}
+        setCompletedCount={setCompletedCount}
+      />
+    </>
+  );
+};
 
 export default Top;
