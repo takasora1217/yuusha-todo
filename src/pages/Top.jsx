ce95f28933596933cef612e1749a4aed9edf4ff7
Iona324 <yori.oto.sai@gmail.com>
2025-05-22 20:37:36 +0900

    fix:todoリストの位置を修正,アニメーションを追加

--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,26 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import InputForm from "../components/InputForm";
 import boss from "../images/boss.png";
 import yuusha from "../images/yuusha-1.png";
 import TodoList from "../components/TodoList";
+
 const Top = () => {
+  const [animatingBoss, setanimatingBoss] = useState(false);
   const [taskList, setTaskList] = useState([]);
   const [completedCount, setCompletedCount] = useState(
     () => JSON.parse(localStorage.getItem("completedCount")) || 0
   );
+  useEffect(() => {
+    if (completedCount > 0) {
+      setanimatingBoss(true);
+      setTimeout(() => {
+        setanimatingBoss(false);
+      }, 1000);
+    }
+  }, [completedCount]);
   return (
     <>
-      <div className="flex flex-col items-center ">
-        <img src={boss} alt="boss" className="w-64" />
-        <InputForm InputForm taskList={taskList} setTaskList={setTaskList} />
-        <img src={yuusha} alt="yuusha" className="w-56" />
-        <div className="flex flex-col space-y-2 m-16">
-          <TodoList
-            taskList={taskList}
-            setTaskList={setTaskList}
-            completedCount={completedCount}
-            setCompletedCount={setCompletedCount}
+      <div className="relative">
+        <div className="flex flex-col items-center ">
+          <img
+            src={boss}
+            alt="boss"
+            className={`w-64 transition-all duration-500 ${
+              animatingBoss ? "nimate-pulse p-4 rounded-md " : ""
+            }`}
           />
+          <InputForm InputForm taskList={taskList} setTaskList={setTaskList} />
+          <img src={yuusha} alt="yuusha" className="w-56" />
+          <div className="absolute top-24 left-16 space-y-2">
+            <TodoList
+              taskList={taskList}
+              setTaskList={setTaskList}
+              completedCount={completedCount}
+              setCompletedCount={setCompletedCount}
+            />
+          </div>
         </div>
       </div>
     </>
