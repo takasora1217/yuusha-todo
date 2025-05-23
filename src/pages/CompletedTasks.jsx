08fbe9e6d062badfc4b83d99d5c4e0a7e22b8d0d
MIYAKOES <miyakokkoa385@gmail.com>
2025-05-23 18:23:47 +0900

    fix:トップページに戻るボタンのかぶり解消

--- src/pages/CompletedTasks.jsx
+++ src/pages/CompletedTasks.jsx
@@ -5,7 +5,7 @@ const CompletedTasks = () => {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className="text-3xl m-20">完了したタスク</div>
+      <div className="text-5xl m-6">完了したタスク</div>
       <div className="max-w-2xl mx-auto">
         {completedTasks.length> 0 ?(
           <ul className="space-y-4">
@@ -32,7 +32,7 @@ const CompletedTasks = () => {
         ))}
       </div>
       <button
-        className="fixed bottom-4 left-1/2 transform -translate-x-1/2 py-2 px-6 border-2 border-black rounded-lg"
+        className="fixed bottom-6 py-2 px-6 border-2 border-black rounded-lg top-0 right-0 w-50 h-16 bg-blue-300 mt-80 mr-40 hover:bg-blue-400"
         onClick={() => (window.location.href = "/")}
       >
         トップページに戻る
