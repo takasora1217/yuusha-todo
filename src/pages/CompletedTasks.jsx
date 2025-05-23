121fe470feb7d8e1b62f9e2a96099b42b12a3950
Iona324 <yori.oto.sai@gmail.com>
2025-05-23 19:09:19 +0900

    feat:完了したタスクの削除機能、completedtasks修正

--- src/pages/CompletedTasks.jsx
+++ src/pages/CompletedTasks.jsx
@@ -1,36 +1,44 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const CompletedTasks = () => {
-  const [completedTasks] = useState(JSON.parse(localStorage.getItem("completedTasks"))||[ ])
+  const [completedTasks, setCompletedtasks] = useState(
+    JSON.parse(localStorage.getItem("completedTasks")) || []
+  );
+
+  const handleDelete = (id) => {
+    setCompletedtasks(completedTasks.filter((task) => task.id !== id));
+  };
 
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="text-3xl m-20">完了したタスク</div>
       <div className="max-w-2xl mx-auto">
-        {completedTasks.length> 0 ?(
+        {completedTasks.length > 0 ? (
           <ul className="space-y-4">
-            {completedTasks.map((task)=>(
-              <li key={task.id}className="flex justify-center border-2 border-black mx-auto w-64">
-                <div className="flex items-center">
-                  <span className="text-gray-700">{task.title}</span>
-                </div>
-              </li>
+            {completedTasks.map((task) => (
+              <div className="flex space-x-2">
+                <li
+                  key={task.id}
+                  className="flex justify-center border-2 border-black mx-auto w-64"
+                >
+                  <div className="flex items-center">
+                    <span className="text-gray-700">{task.title}</span>
+                  </div>
+                </li>
+                <button onClick={() => handleDelete(task.id)}>
+                  <FaTrashAlt />
+                </button>
+              </div>
             ))}
           </ul>
-        ):(
-          <p className="text-center text-gray-500">完了したタスクはありません</p>
+        ) : (
+          <p className="text-center text-gray-500">
+            完了したタスクはありません
+          </p>
         )}
       </div>
 
-      <div className="space-y-2">
-        {completedTasks.map((task)=>(
-          <div
-            key={task.id}
-            className="flex justify-center border-2 border-black mx-auto w-64">
-            {task.title}
-          </div>
-        ))}
-      </div>
       <button
         className="fixed bottom-4 left-1/2 transform -translate-x-1/2 py-2 px-6 border-2 border-black rounded-lg"
         onClick={() => (window.location.href = "/")}
@@ -38,7 +46,7 @@ const CompletedTasks = () => {
         トップページに戻る
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default CompletedTasks
+export default CompletedTasks;
